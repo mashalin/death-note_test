eccc0b5a4072d7481ab8943a54b0f08a1f8c9d7b
mashalin <mashatrushel666@gmail.com>
2021-10-19 02:26:01 +0300

    feat: add indicator

--- js/quiz_code.js
+++ js/quiz_code.js
@@ -3,11 +3,25 @@ window.onload = function() {
     let step = 0;
     let questions = document.querySelector('.question');
     let answerResult = document.querySelector('.answer');
+    let dots = document.querySelector('.dots');
+    let indicator = document.querySelector('.indicator');
+    let number = document.querySelector('.numbers');
 
+    
+
+   for (let i = 0; i < quiz.length; i++) {
+      let dot = document.createElement('div');
+      dot.classList.add('dot');
+      dots.appendChild(dot);
+   }
+   
+   let dot = document.querySelectorAll('.dot');
    
 
     function showQuestion(questionNumber) {
         questions.innerHTML = quiz[step]['q'];
+        dot[step].classList.add('active');
+        number.innerHTML = `${step + 1}/${quiz.length}`;
         let answer = '';
         for (let key in quiz[step]['a']) {
             answer += `<li data-v='${key}' class="answer-variant">${quiz[step]['a'][key]}</li>`
@@ -25,17 +39,17 @@ window.onload = function() {
                 result[event.target.dataset.v] = 0;
             }
             step++;
+            dot[step - 1].classList.remove('active');
             if (step == quiz.length) {
                questions.style.display = 'none';
                answerResult.style.display = 'none';
+               indicator.style.display = 'none';
                 showResult();
             } else {
                 showQuestion(step);
             }
         }
-        console.log(result);
        
-
     }
 
    
@@ -43,7 +57,6 @@ window.onload = function() {
         let key = Object.keys(result).reduce(function(a,b) {
             return result[a] > result[b] ? a : b;
         })
-        console.log(key);
 
         let div = document.createElement('div');
         div.classList.add('result');
@@ -69,6 +82,7 @@ window.onload = function() {
             img.remove();
             questions.style.display = 'block';
             answerResult.style.display = 'block';
+            indicator.style.display = 'flex';
             step = 0;
             result = {};
             button.remove();
